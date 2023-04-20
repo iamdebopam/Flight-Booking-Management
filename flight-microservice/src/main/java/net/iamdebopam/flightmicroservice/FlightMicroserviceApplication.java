d420d570c123006926319b2ad6095f701a64d3c0
iamdebopam <iamdebopam8@gmail.com>
2023-04-20 16:03:02 +0530

    ADDED API GATEWAY

--- flight-microservice/src/main/java/net/iamdebopam/flightmicroservice/FlightMicroserviceApplication.java
+++ flight-microservice/src/main/java/net/iamdebopam/flightmicroservice/FlightMicroserviceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.Calendar;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class FlightMicroserviceApplication  implements CommandLineRunner{
 
 	@Autowired
