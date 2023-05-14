e4167e36946a892cbafbd100d299eb55f9817aea
iamdebopam <iamdebopam8@gmail.com>
2023-05-14 20:04:47 +0530

    INITIAL COMMIT

--- auth-microservice/src/main/java/net/iamdebopam/authmicroservice/model/Role.java
+++
@@ -1,6 +0,0 @@
-package net.iamdebopam.authmicroservice.model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
---
+++ auth/src/main/java/net/iamdebopam/repository/UserCredentialRepository.java
@@ -0,0 +1,11 @@
+package net.iamdebopam.repository;
+
+
+import net.iamdebopam.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCredentialRepository extends JpaRepository<UserCredential,Integer> {
+    Optional<UserCredential> findByName(String username);
+}
